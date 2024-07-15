30d981d6983e1d05f627dce09e28081400e645e2
Ayamarei <ayamarie3102016@gmail.com>
2024-07-15 21:56:34 +0200

    Upload Calc

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("display");
+
+function toDisplay(input) {
+    display.value += input;
+
+  }
+function calculate() {
+    try {
+        display.value=eval(  display.value)
+      
+    } catch (error){
+        display.value="ERROR"
+  }
+}
+function clearDisplay() {
+      display.value=""
+  }
